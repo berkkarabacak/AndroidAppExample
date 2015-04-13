1f1eaa3499ec3ecfb2a8c02df6eea35d9e876d0c
Berk Karabacak <berkkarabacak@gmail.com>
2015-04-13 23:57:58 +0300

    added azure sdk and string connection

--- app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
+++ app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
@@ -4,9 +4,19 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.CloudBlobClient;
+import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.ListBlobItem;
 
 public class Checkactivity extends ActionBarActivity {
 String tcnumara;
+    public static final String storageConnectionString =
+            "DefaultEndpointsProtocol=http;"
+                    + "AccountName=mspproje;"
+                    + "AccountKey='B5lzqD1If9Jf2buT9cnrZJeIf0zgMhkcv4qUsQFObGnFtcs5w7byPP86dHlwILkGeiui4gax9PTSuccIrP4qcA=='";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,5 +30,8 @@ String tcnumara;
         TextView t=(TextView)findViewById(R.id.tcnumaraset);
         t.setText(tcnumara);
 
+
     }
-}
+
+    }
+
