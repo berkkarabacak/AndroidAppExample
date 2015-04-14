afea8e1626c67be8beb219c5110ae31084cb6272
Berk Karabacak <berkkarabacak@gmail.com>
2015-04-14 23:12:26 +0300

    added test systems

--- app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
+++ app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
@@ -6,16 +6,23 @@ import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
 import com.microsoft.azure.storage.CloudStorageAccount;
+import com.microsoft.azure.storage.blob.CloudBlob;
 import com.microsoft.azure.storage.blob.CloudBlobClient;
 import com.microsoft.azure.storage.blob.CloudBlobContainer;
+import com.microsoft.azure.storage.blob.CloudBlockBlob;
 import com.microsoft.azure.storage.blob.ListBlobItem;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+
 
 public class Checkactivity extends ActionBarActivity {
 String tcnumara;
     public static final String ConnectionString =
-            "DefaultEndpointsProtocol=https;"
+            "DefaultEndpointsProtocol=http;"
                     + "AccountName=mspproje;"
-                    + "AccountKey=B5lzqD1If9Jf2buT9cnrZJeIf0zgMhkcv4qUsQFObGnFtcs5w7byPP86dHlwILkGeiui4gax9PTSuccIrP4qcA==";
+                    + "AccountKey=bpBHfSA4uVJVJjxaQcOCKTH7Hib/NAJslvOvnl6q65x652mUahzu6JXKPxAhyP8/w/WKp0htVS5/EVhjaGTkaA==";
 
 
 
@@ -34,33 +41,63 @@ String tcnumara;
 
         try
         {
+            System.out.println("*****************************");
+
+            System.out.println("*****************************");
+
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+
+            System.out.println("*****************************");
+
+
             // Retrieve storage account from connection-string.
        CloudStorageAccount storageAccount = CloudStorageAccount.parse(ConnectionString);
           //  System.out.println(ConnectionString);
+            System.out.println(storageAccount.toString()          );
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
 
             // Create the blob client.
-           CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
 
             // Retrieve reference to a previously created container.
-          CloudBlobContainer container = blobClient.getContainerReference("mspproje");
+            CloudBlobContainer container = blobClient.getContainerReference("mspproje");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println(container.getName());
 
-            // Loop over blobs within the container and output the URI to each of them.
-          for (ListBlobItem blobItem : container.listBlobs()) {
-              System.out.println("*****************************");
-              System.out.println(blobItem.getUri());
-              System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
+            System.out.println("*****************************");
 
-              System.out.println("*****************************");
+            // Loop through each blob item in the container.
+            for (ListBlobItem blobItem : container.listBlobs()) {
+                System.out.println("*****************************");
+                System.out.println("*****************************");
+                System.out.println("*****************************");
+                System.out.println("*****************************");
+                System.out.println(blobItem.toString());
 
-              System.out.println("*****************************");
-              System.out.println("*****************************");
-              System.out.println("*****************************");
-              System.out.println("*****************************");
-
-
-          }
-
-
+                System.out.println("*****************************");
+                System.out.println("*****************************");
+                System.out.println("*****************************");
+                System.out.println("*****************************");                // If the item is a blob, not a virtual directory.
+                if (blobItem instanceof CloudBlob) {
+                    // Download the item and save it to a file with the same name.
+                    CloudBlob blob = (CloudBlob) blobItem;
+                    blob.download(new FileOutputStream("C:\\mydownloads\\" + blob.getName()));
+                }
+            }
         }
         catch (Exception e)
         {
