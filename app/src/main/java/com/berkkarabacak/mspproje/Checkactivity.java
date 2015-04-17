6dd6eee01ee97f39cec4a97056f9d8e74fa04a9d
Berk Karabacak <berkkarabacak@gmail.com>
2015-04-17 18:44:28 +0300

    Refresh method has been adden.(Runnables classes)

--- app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
+++ app/src/main/java/com/berkkarabacak/mspproje/Checkactivity.java
@@ -1,7 +1,9 @@
 package com.berkkarabacak.mspproje;
 
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.widget.TextView;
 
@@ -14,32 +16,26 @@ import com.microsoft.azure.storage.blob.ListBlobItem;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.security.spec.ECField;
 
 
 public class Checkactivity extends ActionBarActivity {
-String tcnumara;
-
-   static TextView t2;
+    String tcnumara;
+    static Integer currentheartbeat;
+    static String currentheartbeatstring;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
         setContentView(R.layout.checkheartbeat);
         Bundle extras = getIntent().getExtras();
-        if(extras !=null)
-        {
+        if (extras != null) {
             tcnumara = extras.getString("tcnumber");
         }
-        TextView t=(TextView)findViewById(R.id.tcnumaraset);
+        TextView t = (TextView) findViewById(R.id.tcnumaraset);
         t.setText(tcnumara);
-
-
-
-
-
-        new Azure().execute();
-
 
     }
 
@@ -48,17 +44,93 @@ String tcnumara;
         super.onResume();
 
         setContentView(R.layout.checkheartbeat);
-        TextView t=(TextView)findViewById(R.id.tcnumaraset);
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) {
+            tcnumara = extras.getString("tcnumber");
+        }
+        TextView t = (TextView) findViewById(R.id.tcnumaraset);
         t.setText(tcnumara);
-        t2=(TextView)findViewById(R.id.currentheartbeat);
-        t2.setText("fdsfsdfsdf");
+
+
+
+
+
+  //  try {
+    //    Thread.sleep(5000);
+
+ //   } catch (Exception e) {
+
+        // asd
+   // }
+        UIUpdater mUIUpdater = new UIUpdater(new Runnable() {
+            @Override
+            public void run() {
+                new Azure().execute();
+
+            }
+        });
+        mUIUpdater.startUpdates();
+        UIUpdater mUIUpdater2 = new UIUpdater(new Runnable() {
+            @Override
+            public void run() {
+                if (currentheartbeat != null) {
+                    currentheartbeatstring = Integer.toString(currentheartbeat);
+                    TextView t2 = (TextView) findViewById(R.id.currentheartbeat);
+                    t2.setText(currentheartbeatstring);
+                }
+            }
+        });
+        mUIUpdater2.startUpdates();
+
 
 
     }
 
-    static public TextView getTextView(){
-        return t2;
+    public class Azure extends AsyncTask<Void, Void, Void> {
+
+        public static final String ConnectionString =
+                "DefaultEndpointsProtocol=http;"
+                        + "AccountName=mspproje;"
+                        + "AccountKey=bpBHfSA4uVJVJjxaQcOCKTH7Hib/NAJslvOvnl6q65x652mUahzu6JXKPxAhyP8/w/WKp0htVS5/EVhjaGTkaA==";
+
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+
+            try
+            {
+
+                // Retrieve storage account from connection-string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.parse(ConnectionString);
+
+                // Create the blob client.
+                CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+                // Retrieve reference to a previously created container.
+                CloudBlobContainer container = blobClient.getContainerReference("mspproje");
+                // Define the path to a local file.
+                final String filePath = "C:\\Users\\Berk\\Desktop\\audi.jpg";
+
+                // Create or overwrite the "myimage.jpg" blob with contents from a local file.
+                CloudBlockBlob blob = container.getBlockBlobReference(MainActivity.tcnumara+".csv");
+                String lastelementdeleted=blob.downloadText().substring(0, blob.downloadText().length() - 1);
+
+                currentheartbeat=Integer.parseInt(lastelementdeleted.substring(lastelementdeleted.lastIndexOf(";")+2));
+                System.out.println(currentheartbeat+"in inner async try");
+            }
+
+
+        catch (Exception e)
+        {
+            // Output the stack trace.
+            // e.printStackTrace();
+        }
+
+
+
+        return null;
+        }
     }
 
-    }
 
+}
